f8ce54b6a1d7194bead9218399cfb91e6e9ebcc4
vikas <vikash4654@gmail.com>
2020-07-21 18:15:36 +0530

    Add project files.

---
+++ FireFoxCmd/ClsListner.cs
@@ -0,0 +1,34 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireFoxCmd
+{
+    class ClsListner
+    {
+        IKeyboardMouseEvents hook;
+        public ClsListner()
+        {
+            hook = Gma.System.MouseKeyHook.Hook.GlobalEvents();
+        }
+        public void ListenMouseMove()
+        {
+            hook.MouseMoveExt += Hook_MouseMoveExt;
+            //hook.MouseMove += OnMouseMove;
+            Console.WriteLine("Attached");
+        }
+
+        private void Hook_MouseMoveExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("Moved");
+        }
+
+        private void OnMouseMove(object sender, MouseEventExtArgs e)
+        {
+
+        }
+    }
+}
---
+++ FireFoxCmd/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace FireFoxCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ClsListner list = new ClsListner();
+            //Thread t = new Thread(list.ListenMouseMove);
+            //t.Start();
+            //Thread.Sleep(3000);
+            //
+            clsActivity act = new clsActivity();
+            act.Start();
+            //Console.WriteLine(Convert.ToInt32(1.2f));
+            Console.ReadLine();
+        }
+    }
+
+    class clsActivity
+    {
+        public Random random = new Random();
+        public int INTERVAL_MILI_SEC = 600*1000;
+ 
+        public int MSeconds()
+        {
+            var dt = DateTime.Now;
+            var sec = dt.Second;
+            sec += (dt.Minute * 60 ) ;
+
+            return (sec * 1000) + dt.Millisecond;
+        }
+        public int Interval()
+        {
+            var mili_sec = MSeconds();
+            return mili_sec % (INTERVAL_MILI_SEC);
+        }
+        public void Start()
+        {
+            InputSimulator simulator = new InputSimulator();
+            var Keyboard = simulator.Keyboard;
+            
+            while (true)
+            {
+                var m_sec = Interval();
+                int min = 1600, max = 2300;
+                var delay = random.Next(min, max); 
+                Console.WriteLine($"downloading... {m_sec/1000}kb. {delay}Mbps..");
+            // If 10min
+                if (m_sec < max)
+                {
+                    Console.WriteLine("0% completed.");
+                    Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.TAB);
+                }
+                if (
+                    (m_sec >100000) && (m_sec < 200000) ||
+                    (m_sec >300000) && (m_sec < 400000)||
+                    (m_sec >500000)
+                    )
+                    Keyboard.KeyPress(VirtualKeyCode.UP);
+                else
+                    Keyboard.KeyPress(VirtualKeyCode.DOWN);
+                Keyboard.Sleep(delay)
+                    .KeyPress(VirtualKeyCode.UP)
+                    .Sleep(10)
+                    .KeyPress(VirtualKeyCode.DOWN);
+            }
+        }
+    }
+}
